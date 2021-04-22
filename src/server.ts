582833e27df981c2ae7eae0f727ac6747cd9911b
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-04-22 17:03:17 -0300

    inicio da aula 04 até o minuto 16

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+
 import './database';
 import { routes } from './routes';
 
 const app = express();
 
+const http = createServer(app); //criando o protocolo http
+const io = new Server(http); //Criando o protocolo ws
+
+io.on('connection', (socket: Socket) => {
+  console.log('Se conectou', socket.id);
+});
+
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('Server is running on port 3333'));
+http.listen(3333, () => console.log('Server is running on port 3333'));

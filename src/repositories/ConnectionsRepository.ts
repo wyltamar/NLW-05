55a927792d7a66afd82be1677ee327bce21f08c9
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-04-22 23:26:31 -0300

    Erro no usersService.findByEmail(email) no arquivo client.ts na pasta websocket

---
+++ src/repositories/ConnectionsRepository.ts
@@ -0,0 +1,6 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { Connection } from '../entities/Connection';
+
+@EntityRepository(Connection)
+class ConnectionsRepository extends Repository<Connection> {}
+export { ConnectionsRepository };
